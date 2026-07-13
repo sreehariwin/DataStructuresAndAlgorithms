2217d47cefe44a1e43ab8cb600a76b57b67faa31
sreehariwin <sreeharikvcloud@gmail.com>
2026-07-13 21:57:12 +0530

    27. Remove Duplicates

--- array.js
+++ array.js
@@ -19,3 +19,53 @@ function removeDuplicates(nums){
 let nums   = [0,0,1,1,1,2,2,3,3,4];
 let result = removeDuplicates(nums);
 
+
+/* 27. Remove Element
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+
+*/
+
+function removeElement(nums, val){
+  
+    let x=0;
+    for(let i=0;i<nums.length;i++){
+       if(nums[i]!=val){
+           nums[x]=nums[i];
+           x=x+1; 
+       }
+    }
+    return x;
+}
+
+let nums = [3,2,1,5,3,4,8,3];
+let val  = 3;
+
+let result = removeElement(nums,val);
+
+console.log(result);
+
+/* 344. Reverse String
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+*/
+
+function reverseString(s){
+  let len = s.length;
+  let halflen = Math.floor(len/2);
+
+  for(let i=0; i<halflen; i++){
+    let sCopy = s[i];
+    s[i] = s[len-1-i];
+    s[len-1-i]=sCopy;
+  }
+
+  return s;
+}
+
+let s = ["h","e","l","l","o"];
+let result = reverseString(s);
+console.log(result);
