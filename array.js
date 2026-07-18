dfe1113155b33b2f190826d06b74a81db34fb003
sreehariwin <sreeharikvcloud@gmail.com>
2026-07-18 22:32:56 +0530

    88. Merge Sorted Array

--- array.js
+++ array.js
@@ -101,4 +101,73 @@ let result = maxProfit(prices);
 
 console.log(result);
 
+/* 88. Merge Sorted Array
 
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+*/
+
+// method 1;
+
+function merge(nums1, m, nums2, n){
+  let nums1Cpy = nums1.slice(0,m);
+  let p1=0;
+  let p2=0;
+  for(let i=0;i<m+n;i++){
+      if(p2 >= n ||(nums1Cpy[p1]<nums2[p2] && p1 < m)){
+        nums1[i] = nums1Cpy[p1];
+        p1++;
+      } else {
+          nums1[i] = nums2[p2];
+          p2++;
+      }
+  }
+
+  return nums1;
+
+}
+
+
+let nums1 = [1,2,3,0,0,0]; 
+let m = 3;
+let nums2 = [2,5,6]; 
+let n = 3;
+
+let result = merge(nums1, m, nums2, n);
+
+console.log(result);
+
+//method 2
+
+function merge(nums1, m, nums2, n){
+
+  let p1 = m-1;
+  let p2 = n-1;
+
+  for(let i=m+n-1;i>=0;i--){
+     
+       if(p2<0){
+           break;
+       }
+    
+       if(p1>=0 && nums1[p1]>nums2[p2]) {
+           nums1[i] = nums1[p1];
+           p1--;
+       } else {
+           nums1[i] = nums2[p2];
+           p2--;
+       }
+   }
+   return nums1;
+}
+ 
+ 
+ let nums1 = [1,2,3,0,0,0]; 
+ let m = 3;
+ let nums2 = [2,5,6]; 
+ let n = 3;
+ 
+ let result = merge(nums1, m, nums2, n);
+ 
+ console.log(result);
