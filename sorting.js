568e0ef72254843783c38abaa2d35f23fc872a18
sreehariwin <sreeharikvcloud@gmail.com>
2026-08-16 11:27:16 +0530

    merge sort

--- sorting.js
+++ sorting.js
@@ -82,3 +82,42 @@ function BubbleSort(nums) {
   
   let result = InsertionSort(nums,9);
   console.log(result);
+
+
+  ?/* Merge Sort
+
+912. Sort an Array
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+
+*/
+
+var sortArray = function(nums) {
+  if(nums.length<=1)return nums;
+  let mid  = Math.floor(nums.length/2);
+  let left = sortArray(nums.slice(0,mid));
+  let right= sortArray(nums.slice(mid));
+  return merge(left,right);
+};
+
+function merge(left,right){
+  let res = [];
+  let i   = 0;    
+  let j   = 0;
+
+  while(i<left.length&&j<right.length){
+      if(left[i]<right[j]){
+          res.push(left[i]);
+          i++;
+      } else {
+          res.push(right[j]);
+          j++;
+      }
+  }
+   
+  return [...res, ...left.slice(i), ...right.slice(j)];
+
+}
+
+let nums = [7,4,3,5,1,2];
+console.log(sortArray(nums));
